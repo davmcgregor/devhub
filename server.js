9577b8ebba6ce84654703d6726eba8514fdd26b1
David McGregor <davidjohnmcgregor@gmail.com>
2020-09-16 21:50:20 +0100

    Create  and connect routes

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+require('dotenv').config()
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('API Running'));
+// Routes
+app.use("/api/auth", require("./routes/api/auth"))
+app.use("/api/posts", require("./routes/api/posts"))
+app.use("/api/profile", require("./routes/api/profile"))
+app.use("/api/users", require("./routes/api/users"))
 
 const port = process.env.PORT || 5000;
 
