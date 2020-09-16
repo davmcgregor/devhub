a994dfb63ed659b6f98abaa17dde6a404e0a86b1
David McGregor <davidjohnmcgregor@gmail.com>
2020-09-16 23:04:53 +0100

    Create user registration route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,20 +1,73 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs'); 
+const jwt = require("jsonwebtoken");
+
+const pool = require("../../db");
 
 router.post('/', [ 
-  body('user_name', 'Name is required').not().isEmpty(),
-  body('user_email', 'Please include a valid email').isEmail(),
-  body('user_password', 'Please enter a password with 6 or more characters').isLength({min: 6})
-], (req, res) => {
+  body('name', 'Name is required').not().isEmpty(),
+  body('email', 'Please include a valid email').isEmail(),
+  body('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
+], 
+async (req, res) => {
 
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
+
+  const { name, email, password } = req.body;
+
+  try {
+
+  // See if user exists
   
-  console.log(req.body);
-  res.send('User route');
+  let user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
+    email
+  ]);
+
+  if (user.rows.length) {
+    return res.status(400).json({ errors: [ { msg: 'User already exists' }]});
+  }
+
+  // Get users gravatar
+
+  const avatar = gravatar.url(email, {
+    s: '200',
+    r: 'pg',
+    d: 'mm'
+  })
+ 
+  // Encrypt Passwprd
+
+  const salt = await bcrypt.genSalt(10);
+  if (!salt) {
+    return res.status(400).json({ errors: [ { msg: 'Something went wrong with bcrypt' }]});
+  }  
+
+  const hash = await bcrypt.hash(password, salt);
+  if (!hash) {
+    return res.status(400).json({ errors: [ { msg: 'Something went wrong hashing the password' }]});
+  }  
+
+  // save User
+
+  const newUser = await pool.query( 
+    "INSERT INTO users (user_name, user_email, user_password, user_avatar) VALUES ($1, $2, $3, $4) RETURNING *",
+    [name, email, hash, avatar]
+  );
+  
+  // return jsonwebtoken
+
+  res.send('User registered');
+    
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error')
+  }
 });
 
 module.exports = router;
