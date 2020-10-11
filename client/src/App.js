f89b10161a56a671c8acf99e1bd1511e5ce1ac63
David McGregor <davidjohnmcgregor@gmail.com>
2020-10-11 13:29:00 +0100

    Consolidate create and edit profiles forms into one ProfileForm

--- client/src/App.js
+++ client/src/App.js
@@ -8,8 +8,7 @@ import Landing from './components/layout/Landing'
 import Alert from './components/layout/Alert'
 import Dashboard from './components/dashboard/Dashboard'
 import PrivateRoute from './components/routing/PrivateRoute'
-import CreateProfile from './components/profile-forms/CreateProfile'
-import EditProfile from './components/profile-forms/EditProfile'
+import ProfileForm from './components/profile-forms/ProfileForm'
 
 // Redux
 import { Provider } from 'react-redux'
@@ -49,12 +48,12 @@ const App = () => {
               <PrivateRoute
                 exact
                 path="/create-profile"
-                component={CreateProfile}
+                component={ProfileForm}
               />
                <PrivateRoute
                 exact
                 path="/edit-profile"
-                component={EditProfile}
+                component={ProfileForm}
               />
             </Switch>
           </section>
